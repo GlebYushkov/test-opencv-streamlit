0f83cf63b1fa31bb361279b1307a666f46e1f1c1
GlebYushkov <72838726+GlebYushkov@users.noreply.github.com>
2022-11-21 17:46:01 +0300

    Update cv2-streamlit.py

--- cv2-streamlit.py
+++ cv2-streamlit.py
@@ -1,6 +1,5 @@
-import cv2
+# import cv2
 import streamlit as st
 from streamlit_webrtc import VideoProcessorBase, webrtc_streamer
-
-st.write('HELLO WORLD')
+# st.write('HELLO WORLD')
 webrtc_streamer(key="example")
